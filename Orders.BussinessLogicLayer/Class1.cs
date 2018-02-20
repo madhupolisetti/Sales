6adc7ac4601b1522dbb654f8fce5cb14a2d6a9ae
Narasimha Murthy Nakka <narasimha.nakka@smscountry.com>
2018-02-20 14:50:09 +0530

    additional classes push

--- Orders.BussinessLogicLayer/Class1.cs
+++ Orders.BussinessLogicLayer/Class1.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OrdersManagement.Core;
-using OrdersManagement.Exceptions;
-using OrdersManagement.Model;
+using Orders.Core;
+using Orders.Exceptions;
+using Orders.Model;
 
 namespace Orders.BussinessLogicLayer
 {
