856ac8f3937975a2e33d21112ed8f833fba38fd7
pujitha Medikonda <pujitha.medikonda@smscountry.com>
2018-02-22 19:08:57 +0530

    Pujitha cghangs

---
+++ Orders.UserDefinedClasses/AccountProducts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.UserDefinedClasses
+{
+    public class AccountProducts
+    {
+
+        private int id = 0;
+        private decimal accountId = 0;
+        private decimal productAccountId = 0;
+        private string productAccountName = string.Empty;
+        private string mobileNo = string.Empty;
+        private string email = string.Empty;
+        private int stateId = 0;
+        private byte countryId = 0;
+        private string address = string.Empty;
+        private string gstin = string.Empty;
+        private int ownerShipId = 0;
+        private bool status = false;
+        private string ownerShipEmail = string.Empty;
+        private byte productId = 0;
+
+        public int Id { get; set; }
+        public decimal AccountId { get; set; }
+        public decimal ProductAccountId { get; set; }
+        public string ProductAccountName { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+        public int StateId { get; set; }
+        public byte CountryId { get; set; }
+        public string Address { get; set; }
+        public string Gstin { get; set; }
+        public int OwnerShipId { get; set; }
+        public bool Status { get; set; }
+        public string OwnerShipEmail { get; set; }
+        public byte ProductId { get; set; }
+
+    }
+}
---
+++ Orders/CommonClasses/Label.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.CommonClasses
+{
+    internal static class Label
+    {
+        internal const string ID = "Id";
+        internal const string DISPLAY_NAME = "DisplayName";
+        internal const string META_DATA_CODE = "MetaDataCode";
+        internal const string ARE_MULTIPLE_ENTRIES_ALLOWED = "AreMultipleEntriesAllowed";
+        internal const string IS_ACTIVE = "IsActive";
+        internal const string TYPE = "Type";
+        internal const string DATA_TYPE = "DataType";
+        internal const string INPUT_TYPE_ID = "InputTypeId";
+        internal const string INPUT_TYPE = "InputType";
+        internal const string INPUT_DATA_TYPE_ID = "InputDataTypeID";
+        internal const string INPUT_DATA_TYPE = "InputDataType";
+        internal const string IS_REQUIRED = "IsRequired";
+        internal const string DEFAULT_VALUE = "DefaultValue";
+        internal const string INCLUDE_IN_ORDER_AMOUNT = "IncludeInOrderAmount";
+        internal const string PROPERTIES = "Properties";
+        internal const string PROPERTY = "Property";
+        internal const string SERVICES = "Services";
+        internal const string SERVICE = "Service";
+        internal const string SERVICE_ID = "ServiceId";
+        internal const string SERVICE_PROPERTY_ID = "ServicePropertyId";
+        internal const string INPUT_TYPES = "InputTypes";
+        internal const string INPUT_DATA_TYPES = "InputDataTypes";
+        internal const string QUOTATION_STATUSES = "QuotationStatuses";
+        internal const string QUOTATION = "Quotation";
+        internal const string QUOTATIONS = "Quotations";
+        internal const string QUOTATION_CHANNELS = "QuotationChannels";
+        internal const string INVOICE_STATUSES = "InvoiceStatuses";
+        internal const string INVOICES = "Invoices";
+        internal const string BILLING_MODES = "BillingModes";
+        internal const string EXTRA_CHARGES = "ExtraCharges";
+        internal const string AMOUNT = "Amount";
+        internal const string VALUE = "Value";
+        internal const string QUOTATION_SERVICES = "QuotationServices";
+        internal const string QUOTATION_SERVICE_PROPERTIES = "QuotationServiceProperties";
+        internal const string QUOTATION_SERVICE_ID = "QuotationServiceId";
+        internal const string OCCURANCE = "Occurance";
+        internal const string PRODUCT_ID = "ProductId";
+
+        internal const string FIELDID = "FieldId";
+        internal const string MINLENGTH = "MinLength";
+        internal const string MAXLENGTH = "MaxLength";
+        internal const string OPTIONS = "Options";
+        internal const string Is_Allow_Special_Chars = "IsAllowSpecialChars";
+        internal const string PROPERTYFIELDS = "PropertyFields";
+
+        internal const string TEXT_BOX = "TextBox";
+        internal const string TEXT_AREA = "TextArea";
+        internal const string DROP_DOWN = "DropDown";
+        internal const string RADIO_BUTTON = "RadioButton";
+        internal const string FILE_UPLOAD = "FileUpload";
+
+        internal const string INT = "Int";
+        internal const string FLOAT = "Float";
+        internal const string STRING = "String";
+        internal const string DATE_TIME = "DateTime";
+        internal const string BIT = "Bit";
+        internal const string MONEY = "Money";
+
+        internal const string SUCCESS = "Success";
+        internal const string MESSAGE = "Message";
+        internal const string OUTPUT_PARAMETERS = "OutputParameters";
+        internal const string CONNECTION_STRING_NAME = "OrdersDbConnectionString";
+
+        internal const string CREATE = "Create";
+        internal const string UPDATE = "Update";
+
+        #region LOG_4_NET_RELATED
+        internal const string LOG_NAME = "LogName";
+        internal const string DEFAULT_LOGGER = "DefaultLogger";
+        internal const string TRACE_LOGGER = "TraceLogger";
+        internal const string LOG_FILE_FORMAT = "yyyyMMdd";
+        #endregion
+    }
+}
