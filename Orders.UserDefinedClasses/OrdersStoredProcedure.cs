50c0eb918b593288ae3e8f156bbfb30ea2a64b8f
pujitha Medikonda <pujitha.medikonda@smscountry.com>
2018-02-22 13:34:24 +0530

    Push chnages

---
+++ Orders.UserDefinedClasses/OrdersStoredProcedure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json.Linq;
+namespace Orders.UserDefinedClasses
+{
+    public static class StoredProcedure
+    {
+
+        #region PRIVATE VARIABLES
+
+        private string _connectionString = string.Empty;
+        private SqlConnection _sqlConnection = null;
+        private SqlCommand _sqlCommand = null;
+        private SqlDataAdapter _da = null;
+        private DataSet _ds = null;
+        private JObject _jObj = null;
+        private JArray _jArr = null;
+
+        #endregion
+        #region Products Related
+        public const string GET_PRODUCT_DETAILS = "GetProductDetails";
+        #endregion
+
+
+    }
+}
