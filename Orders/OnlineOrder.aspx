b1bdfc83f38a81b23c0387db47bad9a6aab86788
anu <anudeep.engala@smscountry.com>
2020-02-26 11:47:16 +0530

    dfgveb

---
+++ Orders/OnlineOrder.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OnlineOrder.aspx.cs" Inherits="Orders.OnlineOrder" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <button id="rzp-button1">Pay</button>
+        </div>
+    </form>
+</body>
+<script
+    src="https://checkout.razorpay.com/v1/checkout.js"
+    data-key="rzp_test_NM2snZJrPHBYS4"
+    data-amount="100"
+    data-name="Razorpay"
+    data-description="Purchase Description"
+    data-order_id="<%=orderId%>"
+    data-image="https://www.grptalk.com/images/logo-grp.png"
+    data-prefill.name="Gaurav Kumar"
+    data-prefill.email="gaurav.kumar@example.com"
+    data-prefill.contact="9123456789"
+    data-theme.color="#007bff"
+></script>
+
+</html>
---
+++ Orders/OnlineOrder.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Razorpay.Api;
+using Razorpay.Api.Errors;
+
+namespace Orders
+{
+    public partial class OnlineOrder : System.Web.UI.Page
+    {
+        public string orderId;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            Dictionary<string, object> input = new Dictionary<string, object>();
+            input.Add("amount", 100); // this amount should be same as transaction amount
+            input.Add("currency", "INR");
+            input.Add("receipt", "12121");
+            input.Add("payment_capture", 1);
+
+            string key = "rzp_test_NM2snZJrPHBYS4";
+            string secret = "pVIY3RofRBiPHe9lSSL5RafE";
+
+            RazorpayClient client = new RazorpayClient(key, secret);
+
+            Razorpay.Api.Order order = client.Order.Create(input);
+            orderId = order["id"].ToString();
+        }
+    }
+}
